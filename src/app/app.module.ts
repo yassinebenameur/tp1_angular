b39961f7e42d94d4ac1111390c0eaccf06305431
Yassine ben ameur <superyassine15@gmail.com>
2019-10-04 14:45:16 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CvComponent } from './cv/cv.component';
+import { ListeComponent } from './liste/liste.component';
+import { ItemComponent } from './item/item.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    ListeComponent,
+    ItemComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
